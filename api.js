20c3cc855341bfb900d820dd1e6357c1f4f943f5
maartenkeizer <68481866+maartenkeizer@users.noreply.github.com>
2023-07-18 13:41:28 +0200

    replacing miragejs with firebase

--- api.js
+++ api.js
@@ -1,3 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite"
+ // TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYSNucXmWeX50xPl_YUdNpce43S1IPwl4",
+  authDomain: "onlyvans-db2ca.firebaseapp.com",
+  projectId: "onlyvans-db2ca",
+  storageBucket: "onlyvans-db2ca.appspot.com",
+  messagingSenderId: "353315542629",
+  appId: "1:353315542629:web:29e030e1d0253614f9588c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+
+
+
+
+
+
 export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
